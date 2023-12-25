c88020b1353660a8bbca6b2ee25f0502a8de7b51
johnakisk0700 <johnnstavv@gmail.com>
2023-12-25 03:41:22 +0200

    [Refactor] Changed Employees to Users and added more columns

--- src/main/java/com/adeies/adeies/enterprise/model/Employee.java
+++ src/main/java/com/adeies/adeies/enterprise/model/Employee.java
@@ -7,26 +7,13 @@ import java.util.Date;
 
 @Data
 @Entity
-@Table( name = "EMPLOYEE")
+@Table(name = "EMPLOYEE")
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "EMPLOYEE_ID")
+    @Column(name = "EMPLOYEE_ID")
     private Long employee_id;
 
-    @Column (name = "FIRST_NAME")
-    private String firstname;
 
-    @Column(name = "LAST_NAME")
-    private String lastname;
-
-    private String email;
-    @Column( name = "passwords")
-    private String passwords;
-    @Column(name = "birthdate")
-    private Date birthdate;
-
-    @Column(name = "marital_status",length = 30)
-    private String maritalStatus;
 }

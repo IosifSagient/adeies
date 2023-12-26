81d463904381bbfbbde9b45fade6f6c80d7c1002
iosif <iosif.sagient@gmail.com>
2023-12-26 22:04:09 +0200

    25/12/2023 : SearchEmployee

--- src/main/java/com/adeies/adeies/enterprise/mappers/UpdateEmployeeMapper.java
+++ src/main/java/com/adeies/adeies/enterprise/mappers/UpdateEmployeeMapper.java
@@ -1,20 +1,24 @@
 package com.adeies.adeies.enterprise.mappers;
+
 import com.adeies.adeies.enterprise.dto.EmployeeDto;
 import com.adeies.adeies.enterprise.model.Employee;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
 
 @Component
-@Mapper( nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE )
+@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UpdateEmployeeMapper {
 
     UpdateEmployeeMapper INSTANCE = Mappers.getMapper(UpdateEmployeeMapper.class);
 
-    @Mapping(target = "employeeId",ignore = true)
+    @Mapping(target = "employeeId", ignore = true)
     EmployeeDto toDto(Employee employee);
 
     @Mapping(target = "employeeId", ignore = true)
-    Employee toEntity(EmployeeDto dto,  @MappingTarget Employee currentEmployee);
+    Employee toEntity(EmployeeDto dto, @MappingTarget Employee currentEmployee);
 }

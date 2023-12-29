0a9cacbd7a8cf1a40b9cbeb9270bdfca7bfb081d
iosif <iosif.sagient@gmail.com>
2023-12-29 04:09:00 +0200

    [Feature] : Create day off definition, Create Day Off, Small Changes in some entities.

--- src/main/java/com/adeies/adeies/enterprise/pojos/DaysOffAvailablePK.java
+++ src/main/java/com/adeies/adeies/enterprise/pojos/DaysOffAvailablePK.java
@@ -1,7 +1,5 @@
 package com.adeies.adeies.enterprise.pojos;
 
-import com.adeies.adeies.enterprise.entities.DaysOffDefinition;
-import com.adeies.adeies.enterprise.entities.User;
 import lombok.Data;
 import java.io.Serializable;
 
